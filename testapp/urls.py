231088aa1d54ed56e000687adf386b2ee1f0f4fd
Patrick Szczepanski <p.szczepanski996@gmail.com>
2022-10-14 12:37:05 +0200

    Small fixes

--- testapp/urls.py
+++ testapp/urls.py
@@ -7,6 +7,6 @@ from testapp.views import TestPaypalView
 from testapp.views import paypal_ipn
 
 urlpatterns = [
-    path('test/paypal/', TestPaypalView.as_view(), name='test_paypal'),
+    path('test-paypal/', TestPaypalView.as_view(), name='test_paypal'),
     path('paypal-ipn/', paypal_ipn, name='paypal_ipn'),
 ]
