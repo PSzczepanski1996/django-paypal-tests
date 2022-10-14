7b5ff1b1bc8f87e54482e4d496bcaa9aa3e9d82d
Patrick Szczepanski <p.szczepanski996@gmail.com>
2022-10-14 12:26:40 +0200

    Register PaypalResponse admin class

--- testapp/admin.py
+++ testapp/admin.py
@@ -2,7 +2,11 @@
 # Django
 from django.contrib import admin
 
+# Project
+from testapp.models import PaypalResponse
 
+
+@admin.register(PaypalResponse)
 class PaypalResponseAdmin(admin.ModelAdmin):
     """Paypal response admin."""
 
